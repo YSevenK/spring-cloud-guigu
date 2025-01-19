78f551fb1764dd983c0c7798dfd5fb1b6aeb380a
ysevenk <ysevenk@qq.com>
2025-01-19 16:05:58 +0800

    nacos配置中心基本使用

--- services/service-order/src/main/java/com/ysevenk/order/controller/OrderController.java
+++ services/service-order/src/main/java/com/ysevenk/order/controller/OrderController.java
@@ -3,19 +3,33 @@ package com.ysevenk.order.controller;
 import com.ysevenk.order.bean.Order;
 import com.ysevenk.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope   //自动刷新nacos配置
 @RestController
 public class OrderController {
     @Autowired
     OrderService orderService;
 
+    @Value("${order.timeout}")
+    String orderTimeout;
+
+    @Value("${order.auto-confirm}")
+    String orderAutoConfirm;
+
+    @GetMapping("/config")
+    public String config() {
+        return "order.timeout=" + orderTimeout + "; order-confirm=" + orderAutoConfirm;
+    }
+
     // 创建订单
     @GetMapping("/create")
     public Order createOrder(@RequestParam("userId") Long userId,
-                             @RequestParam("productId") Long productId){
+                             @RequestParam("productId") Long productId) {
         Order order = orderService.createOrder(productId, userId);
         return order;
     }
